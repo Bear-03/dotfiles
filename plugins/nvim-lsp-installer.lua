be4a29d9286f6c854f0f5059dcd40d5b2f85a785
Bear-03 <64696287+Bear-03@users.noreply.github.com>
2022-05-29 14:48:01 +0200

    Ensure LSP installation

---
+++ plugins/nvim-lsp-installer.lua
@@ -0,0 +1,14 @@
+return {
+    automatic_installation = true,
+    ensure_installed = {
+        "rust_analyzer",
+        "sumneko_lua",
+        "taplo", -- TOML
+        "cssls",
+        "cssmodules_ls",
+        "emmet_ls",
+        "html",
+        "jsonls",
+        "tsserver",
+    }
+}
