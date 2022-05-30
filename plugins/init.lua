1edadc925cf78792470813f223fc24e7e47e6c3d
Bear-03 <64696287+Bear-03@users.noreply.github.com>
2022-05-31 01:20:31 +0200

    Fix crates.nvim

--- plugins/init.lua
+++ plugins/init.lua
@@ -32,6 +32,9 @@ return {
         "saecki/crates.nvim",
         event = { "BufRead Cargo.toml" },
         requires = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("crates").setup()
+        end
     },
     { -- Github copilot
         "github/copilot.vim",
