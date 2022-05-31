03166c67e359062194280f1bea2c144a50a143a4
Bear-03 <64696287+Bear-03@users.noreply.github.com>
2022-05-31 15:08:28 +0200

    Merge gruvqueen palettes

--- plugins/config/gruvqueen_config.lua
+++ plugins/config/gruvqueen_config.lua
@@ -8,16 +8,16 @@ return function ()
         },
     })
 
-    local palette = require("gruvqueen.palette").get_dark_theme_palette()
-    local common_palette = palette.common
-    local style_palette = palette[style]
+    -- Palette comes split into common, material, mix and original
+    local split_palette = require("gruvqueen.palette").get_dark_theme_palette()
+    local palette = vim.tbl_deep_extend("force", split_palette.common, split_palette[style])
 
-    vim.api.nvim_set_hl(0, "TSParameter", { fg = style_palette.blue })
-    vim.api.nvim_set_hl(0, "TSField", { fg = style_palette.fg0 }) -- Attributes
-    -- -- vim.api.nvim_set_hl(0, "TSFunction", { fg = style_palette.fg0 })
+    vim.api.nvim_set_hl(0, "TSParameter", { fg = palette.blue })
+    vim.api.nvim_set_hl(0, "TSField", { fg = palette.fg0 }) -- Attributes
+    -- vim.api.nvim_set_hl(0, "TSFunction", { fg = palette.fg0 })
     vim.api.nvim_set_hl(0, "TSConstant", { link = "TSNumber" })
     vim.api.nvim_set_hl(0, "TSConstBuiltin", { link = "TSConstant" })
-    vim.api.nvim_set_hl(0, "TSVariableBuiltin", { fg = common_palette.grey1 }) -- ex. self / this
-    vim.api.nvim_set_hl(0, "rustTSPunctBracket", { fg = style_palette.red }) -- For closures in rust
-    vim.api.nvim_set_hl(0, "TSOperator", { fg = style_palette.aqua })
+    vim.api.nvim_set_hl(0, "TSVariableBuiltin", { fg = palette.grey1 }) -- ex. self / this
+    vim.api.nvim_set_hl(0, "rustTSPunctBracket", { fg = palette.red }) -- For closures in rust
+    vim.api.nvim_set_hl(0, "TSOperator", { fg = palette.aqua })
 end
