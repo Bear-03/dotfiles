09160dd95220c38c03df3db016337f128da09d12
Bear-03 <64696287+Bear-03@users.noreply.github.com>
2022-06-18 15:07:25 +0200

    Update rust-tool options

--- lsp/server_registration.lua
+++ lsp/server_registration.lua
@@ -2,11 +2,9 @@ return function(server, opts)
     if server == "rust_analyzer" then
         local custom_opts = {
             tools = {
-                hover_with_actions = true,
                 inlay_hints = {
-                    show_parameter_hints = false,
                     parameter_hints_prefix = "",
-                    other_hints_prefix = "",
+                    other_hints_prefix = "-> ",
                 },
             },
             server = vim.tbl_deep_extend("force", opts, {
