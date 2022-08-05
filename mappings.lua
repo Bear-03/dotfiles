0142f2219725681e7e4c0a9ef31fa83998ee4096
Bear-03 <64696287+Bear-03@users.noreply.github.com>
2022-08-05 20:55:16 +0200

    Treat wrapped lines as normal with j and k

--- mappings.lua
+++ mappings.lua
@@ -14,3 +14,9 @@ end
 
 -- Neogit mapping (Overrides toggle-term LazyGit binding)
 vim.keymap.set("n", "<leader>gg", "<cmd>Neogit<CR>", { noremap = true })
+
+-- Treat wrapped lines as normal lines with j and k
+-- Note: for simplicity, this is only set to work with
+-- j and k, so dd, yy etc won't work
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
