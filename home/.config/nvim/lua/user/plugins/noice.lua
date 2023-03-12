7ce30457284b7add93b4e8a23e069c2b5707740b
Bear-03 <64696287+Bear-03@users.noreply.github.com>
2023-03-12 14:06:08 +0100

    [nvim] Move noice config to another file

---
+++ home/.config/nvim/lua/user/plugins/noice.lua
@@ -0,0 +1,18 @@
+return {
+    { import = "astrocommunity.utility.noice-nvim" },
+    {
+        "noice.nvim",
+        opts = {
+            views = {
+                mini = {
+                    border = {
+                        style = "rounded",
+                    },
+                    win_options = {
+                        winblend = 0
+                    }
+                },
+            },
+        }
+    },
+}
--- home/.config/nvim/lua/user/plugins/nosetup.lua
+++ home/.config/nvim/lua/user/plugins/nosetup.lua
@@ -4,22 +4,6 @@ return {
     {
         "AstroNvim/astrocommunity"
     },
-    { import = "astrocommunity.utility.noice-nvim" },
-    {
-        "noice.nvim",
-        opts = {
-            views = {
-                mini = {
-                    border = {
-                        style = "rounded",
-                    },
-                    win_options = {
-                        winblend = 0
-                    }
-                },
-            },
-        }
-    },
     -- Treesitter debugging
     {
         "nvim-treesitter/playground",
