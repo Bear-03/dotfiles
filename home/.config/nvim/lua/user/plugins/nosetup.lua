9156abac41a79db9fa0061dfe0d76360ac406af5
Bear-03 <64696287+Bear-03@users.noreply.github.com>
2023-03-12 14:42:44 +0100

    [nvim] Add tabout plugin

--- home/.config/nvim/lua/user/plugins/nosetup.lua
+++ home/.config/nvim/lua/user/plugins/nosetup.lua
@@ -1,5 +1,15 @@
 -- Small plugins that require no configuration
 return {
+    -- Exit strings and delimiters with TAB
+    {
+        "abecodes/tabout.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "hrsh7th/nvim-cmp",
+        },
+        config = true,
+    },
     -- Treesitter debugging
     {
         "nvim-treesitter/playground",
