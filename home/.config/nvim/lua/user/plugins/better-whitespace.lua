a2848c5eed8fa4ff9228d15a9765d75641bf0912
Bear-03 <64696287+Bear-03@users.noreply.github.com>
2023-03-12 14:43:19 +0100

    [nvim] Turn off better-whitespace on certain files

--- home/.config/nvim/lua/user/plugins/better-whitespace.lua
+++ home/.config/nvim/lua/user/plugins/better-whitespace.lua
@@ -6,5 +6,6 @@ return {
         vim.g.better_whitespace_enabled = true
         vim.g.strip_whitespace_on_save = true
         vim.g.strip_whitespace_confirm = false
+        vim.g.better_whitespace_filetypes_blacklist = { "alpha", "neo-tree", "toggleterm" }
     end
 }
