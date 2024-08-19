6ea6cac9afb02ebe6f2e59ce653ecb61b65ad5d7
Bear-03 <64696287+Bear-03@users.noreply.github.com>
2024-08-20 01:01:50 +0200

    [pi] Test grub error 3

--- hosts/bear-pi/nixos/configuration.nix
+++ hosts/bear-pi/nixos/configuration.nix
@@ -1,6 +1,4 @@
-{ config, pkgs, usernames, hostname, flakeRoot, ... } @ inputs:
-{
-    # Edit this configuration file to define what should be installed on
+# Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
