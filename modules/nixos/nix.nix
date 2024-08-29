9ed98bb16b75f56cc0f476d52919b0f2687d0b4e
Bear-03 <64696287+Bear-03@users.noreply.github.com>
2024-08-29 19:22:45 +0200

    Add trusted users to nix

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -3,6 +3,7 @@
         settings = {
             auto-optimise-store = true;
             experimental-features = [ "nix-command" "flakes" ];
+            trusted-users = [ "@wheel" ];
         };
         gc = {
             automatic = true;
