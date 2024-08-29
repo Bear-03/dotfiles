a57f0d75c4677c809cda8a13ce537308141ece65
Bear-03 <64696287+Bear-03@users.noreply.github.com>
2024-08-29 20:21:02 +0200

    [pi] Try to make remote builds work 2

--- hosts/bear-pi/nixos/ssh.nix
+++ hosts/bear-pi/nixos/ssh.nix
@@ -3,25 +3,12 @@
     services.openssh.enable = true;
 
     # Allows remote nixos-rebuild without loging in as root
-    # Workaround for https://github.com/NixOS/nixpkgs/issues/118655
-    # Source: https://discourse.nixos.org/t/remote-nixos-rebuild-sudo-askpass-problem/28830/15
-    security.sudo.extraRules = let
-        storePrefix = "/nix/store/*";
-        systemName = "nixos-system-${hostname}-*";
-    in [
-        {
-            groups = [ "wheel" ];
-            commands = [ {
-                command = "${storePrefix}-nix-*/bin/nix-env -p /nix/var/nix/profiles/system --set ${storePrefix}-${systemName}";
-                options = [ "NOPASSWD" ];
-            } ];
-        }
-        {
-            groups = [ "wheel" ];
-            commands = [ {
-                command = "${storePrefix}-${systemName}/bin/switch-to-configuration";
-                options = [ "NOPASSWD" ];
-            } ];
-        }
-    ];
+    # Source: https://github.com/NixOS/nixpkgs/issues/118655#issuecomment-1537131599
+    security.sudo.extraRules = [ {
+        groups = [ "wheel" ];
+        commands = [ {
+            command = "/run/current-system/sw/bin/nix-store";
+            options = [ "NOPASSWD" ];
+        } ];
+    } ];
 }
