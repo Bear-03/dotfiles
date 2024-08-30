1d6013ba541de65152506257d1d0840512e17b71
Bear-03 <64696287+Bear-03@users.noreply.github.com>
2024-08-30 21:05:31 +0200

    [pi - jellyfin] Export provisional ports needed for internal network access

--- hosts/bear-pi/nixos/caddy.nix
+++ hosts/bear-pi/nixos/caddy.nix
@@ -2,7 +2,17 @@ let
     domain = "192.168.1.151"; # TODO: Change to domain
 in
 {
-    networking.firewall.allowedTCPPorts = [ 80 443 ];
+    networking.firewall.allowedTCPPorts = [
+        80 # HTTP
+        443 # HTTPS
+        8096 # Jellyfin TODO: Remove, only useful because some clients dont let you use IPs without that exact port
+    ];
+
+    networking.firewall.allowedUDPPorts = [
+        1900 # Jellyfin service discovery
+        7359 # Jellyfin client discovery
+    ];
+
     services.caddy = {
         enable = true;
         # Set up "jellyfin." subdomain when it is changed to an actual domain
