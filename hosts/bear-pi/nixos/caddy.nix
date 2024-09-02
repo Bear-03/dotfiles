dec38a5be0c9d9c8985cdf39b5da972a824bc617
Bear-03 <64696287+Bear-03@users.noreply.github.com>
2024-09-02 18:55:17 +0200

    [pi] Add domain name to caddy

--- hosts/bear-pi/nixos/caddy.nix
+++ hosts/bear-pi/nixos/caddy.nix
@@ -1,24 +1,22 @@
 let
-    domain = "192.168.1.151"; # TODO: Change to domain
+    domain = "bearspi.duckdns.org";
 in
 {
     networking.firewall.allowedTCPPorts = [
         80 # HTTP
         443 # HTTPS
-        8096 # Jellyfin TODO: Remove, only useful because some clients dont let you use IPs without that exact port
     ];
 
     networking.firewall.allowedUDPPorts = [
-        1900 # Jellyfin service discovery
-        7359 # Jellyfin client discovery
+        # Jellyfin server discovery within private network
+        1900 # Service discovery
+        7359 # Client discovery
     ];
 
     services.caddy = {
         enable = true;
-        # Set up "jellyfin." subdomain when it is changed to an actual domain
-        virtualHosts."${domain}".extraConfig = ''
+        virtualHosts."jelly.${domain}".extraConfig = ''
             reverse_proxy localhost:8096
-            tls internal # Change when using an actual domain
         '';
     };
 }
