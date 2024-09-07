c772984d56bea89aed968659bda40ab01df22a99
Bear-03 <64696287+Bear-03@users.noreply.github.com>
2024-09-07 15:04:56 +0200

    [laptop] Formatting

--- hosts/bear-laptop/nixos/configuration.nix
+++ hosts/bear-laptop/nixos/configuration.nix
@@ -28,12 +28,12 @@
             logReversePathDrops = true;
             # Wireguard trips rpfilter up
             extraCommands = ''
-              ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
-              ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
+                ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
+                ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
             '';
             extraStopCommands = ''
-              ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
-              ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
+                ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
+                ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
             '';
         };
     };
